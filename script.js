834e10986cd3cb5c1da2bee3861da9f355330d57
godtelesman-frost <godtelesman@gmail.com>
2026-05-14 07:51:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+console.log("EchoMind Loaded Successfully");
 
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+  button.addEventListener("mouseenter", () => {
+    button.style.transform = "scale(1.05)";
+  });
+
+  button.addEventListener("mouseleave", () => {
+    button.style.transform = "scale(1)";
+  });
+
+});
