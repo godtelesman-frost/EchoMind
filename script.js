91aefbbf5e852f371a26c1963969a2c8c430b00c
godtelesman-frost <godtelesman@gmail.com>
2026-05-25 14:10:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -733,7 +733,7 @@ async function sendMessage(){
     const response =
     await fetch(
 
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + API_KEY,
 
       {
 
